8ce4e0d87b7883d80daf37b1119012761e10b659
Ayna Khajawal <121284130+ayna-zargar@users.noreply.github.com>
2025-10-17 12:47:30 +0530

    completed UTXO
    1->we ensure there is sufficient am ount IP<OP
    2->made sure IPUTXOs are marked spent after they are spent
    3->calculated miners fee;

--- UTXO/Transaction.js
+++ UTXO/Transaction.js
@@ -4,8 +4,21 @@ class Transaction {
     this.outputTXOs = outputTXOs;
   }
   execute() {
+    const TotalInputAmount = this.inputUTXOs.reduce((p, c) => {
+      return p + c.amount, 0;
+    });
+    const TotalOutputAmount = this.outputTXOs.reduce((p, c) => {
+      return p + c.amount, 0;
+    });
+
     if (this.inputUTXOs.some((x) => x.spent)) {
       throw new Error("Input UTXOs already spent");
     }
+    if (TotalInputAmount < TotalOutputAmount) {
+      throw new Error("Input amount is less than output amount");
+    }
+    this.inputUTXOs.forEach((x) => x.spend());
+    this.fee = TotalInputAmount - TotalOutputAmount;
   }
 }
+Module.exports = Transaction;
